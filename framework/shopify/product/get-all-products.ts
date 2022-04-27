dacd2aff2ddb39fa2417e23cf1468cbf46aea70e
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-04-27 21:09:11 +0700

    used the grapghql post method

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,12 +1,20 @@
-const fetchData = async () => {
-  const data = await (
-    await fetch("https://jsonplaceholder.typicode.com/todos")
-  ).json();
+import getAllProductsQuery from "../utils/queries/get-all-products";
+const fetchData = async ({ query }: {query: string}) => {
+  const url = "http://localhost:4000/graphql";
+  const data = await (await fetch(url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query,
+    })
+  })).json();
   return { data };
 };
 
 const getAllProducts = async (): Promise<any[]> => {
-  const { data } = await fetchData();
+  const { data } = await fetchData({ query: getAllProductsQuery });
   return data;
 };
 
