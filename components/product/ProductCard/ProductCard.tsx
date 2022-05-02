e1fb02d8947a85ac33f5cdc4040016bfff8f8483
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-05-02 22:33:10 +0700

    added link and image

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,15 +1,36 @@
 import { IProduct } from "@common/types/products";
+import Image from "next/image";
+import Link from "next/link";
 
 interface IProps {
   product: IProduct
 }
 
-
+const placeholderImage = '/product-image-placeholder.svg';
 const ProductCard: React.FC<IProps> = ({ product }) => {
   return (
-    <div>
-      {product.name}
-    </div>
+    <Link href={`/products/${product.slug}`} passHref>
+      <a>
+        <div>
+          <h3>
+            <span>{product.name}</span>
+          </h3>
+          <span>18 $</span>
+        </div>
+        { 
+          product.images && (
+            <Image
+              src={placeholderImage}
+              height={540}
+              width={540}
+              quality={85}
+              layout="responsive"
+              alt={product.name ?? "Product image"}
+            />
+          )
+        }
+      </a>
+    </Link>
   )
 }
 
