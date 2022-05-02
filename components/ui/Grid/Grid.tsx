c492a60f26ecb8450bc34b6247d2bfe18498c9e4
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-05-03 02:08:24 +0700

    used the 'classnames' library to render classes conditionally

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import style from "./Grid.module.css";
+import cn from 'classnames';
 
-const Grid: React.FC<{children: React.ReactNode}> = ({children}) => {
+interface IProps {
+  children: React.ReactNode[];
+  layout?: 'A' | 'B';
+}
+
+const Grid: React.FC<IProps> = ({children, layout = 'A'}) => {
+  const rootClassName = cn(style.root, {
+    [style.layoutA]: layout === "A",
+    [style.layoutB]: layout === "B"
+  })
+
   return (
-    <div className={style.root}>
+    <div className={rootClassName}>
       {children}
     </div>
   )
