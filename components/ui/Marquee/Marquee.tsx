427e6993634a4def00b336e8296636241f2d936c
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-05-10 22:34:17 +0700

    used conditional styling to style bg of marquee

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,13 +1,21 @@
 import { Ticker } from '@components/ui';
 import style from "./Marquee.module.css";
+import cn from 'classnames';
 
 interface IProps {
+  variant?: 'primary' | 'secondary';
   children: Array<React.ReactNode>;
 }
 
-const Marquee: React.FC<IProps> = ({ children }) => {
+const Marquee: React.FC<IProps> = ({ children, variant = 'primary' }) => {
+  const rootClassName = cn(
+    style.root,
+    {
+      [style.secondary]: variant === 'secondary'
+    }
+  )
   return (
-    <div className={style.root}>
+    <div className={rootClassName}>
       <Ticker height={40} offset={80} length={children.length}>
         <div className={style.container}>{children}</div>
       </Ticker>
