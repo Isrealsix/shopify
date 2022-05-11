b5c0c1e777f09424cbe4711a51311621e3f8a5be
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-05-11 22:39:25 +0700

    consumed the UIProvider state

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,19 +1,22 @@
-import { Navbar, Footer } from '@components/common';
-import { Sidebar } from '@components/ui';
-import { CartSidebar } from '@components/cart';
+import { Navbar, Footer } from "@components/common";
+import { Sidebar } from "@components/ui";
+import { CartSidebar } from "@components/cart";
+import { useUI } from "@components/ui/context";
 import styles from "./Layout.module.css";
 
-const Layout: React.FC<{children: React.ReactNode}> = ({ children }) => (
-  <div className={styles.root}>
-    <Navbar />
-    <Sidebar>
-      <CartSidebar />
-    </Sidebar>
-    <main className="fit">
-      {children}
-    </main>
-    <Footer />
-  </div>
-)
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const ui = useUI();
+  console.log(ui);
+  return (
+    <div className={styles.root}>
+      <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
+      <main className="fit">{children}</main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,25 @@
 import "assets/main.css";
+import { UIProvider, useUI } from "@components/ui/context";
 import { AppProps } from "next/app";
 import React from "react";
 
-type ReactFC = React.FC<{children: React.ReactNode}>;
-const LayoutFallback: ReactFC = ({children}) => <> {children} </>;
-
-
-const MyApp = ({Component, pageProps}: AppProps & {Component: {Layout: ReactFC}}) => {
+type ReactFC = React.FC<{ children: React.ReactNode }>;
+const LayoutFallback: ReactFC = ({ children }) => <> {children} </>;
+const MyApp = ({
+  Component,
+  pageProps,
+}: AppProps & { Component: { Layout: ReactFC } }) => {
   const Layout = Component.Layout ?? LayoutFallback;
+  const ui = useUI();
+  console.log(ui);
+  // When we use the useUI outside a component that is not wrapped by the UIProvider, we only get the default state
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+    <UIProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UIProvider>
+  );
+};
 
 export default MyApp;
