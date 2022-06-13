adb21920c3250956701d524821fa217e9d212f95
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-06-14 00:54:20 +0700

    Get product query

--- framework/shopify/product/get-product.ts
+++ framework/shopify/product/get-product.ts
@@ -1,6 +1,12 @@
 import { IApiConfig } from "@common/types/api";
+import { getProductQuery } from "@framework/utils";
 
 const getProduct = async (config: IApiConfig): Promise<any> => {
+  const { data } = await config.fetch<any>({
+    query: getProductQuery,
+    url: config.apiUrl,
+  });
+  console.log(JSON.stringify(data, null, 2));
   return {
     product: {
       name: "our super product",
