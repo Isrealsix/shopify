0cf8af82a827e5e14c4a40768a1bcc342c61fdae
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-06-13 22:45:05 +0700

    get static paths query

--- framework/shopify/product/get-all-products-paths.ts
+++ framework/shopify/product/get-all-products-paths.ts
@@ -1,18 +1,23 @@
 import { IApiConfig } from "@common/types/api";
 import { IProduct } from "@common/types/products";
+import { ProductConnection } from "@framework/schema";
+import getAllProductPathsQuery from "@framework/utils/queries/get-all-products-paths";
 
 type ReturnType = {
   products: Pick<IProduct, "slug">[];
 };
 
 const getAllProductsPaths = async (config: IApiConfig): Promise<ReturnType> => {
-  return {
-    products: [
-      { slug: "cool-hat" },
-      { slug: "t-shirt" },
-      { slug: "lightweight-jacket" },
-    ],
-  };
+  const { data } = await config.fetch<{ products: ProductConnection }>({
+    query: getAllProductPathsQuery,
+    url: config.apiUrl,
+  });
+  const products = data.products.edges.map(({ node: { handle } }) => {
+    return {
+      slug: handle,
+    };
+  });
+  return { products };
 };
 
 export default getAllProductsPaths;
---
+++ framework/shopify/utils/queries/get-all-products-paths.ts
@@ -0,0 +1,13 @@
+const getAllProductPathsQuery = `
+  query getAllProductsPaths($first: Int = 250) {
+    products(first: $first) {
+      edges {
+        node {
+          handle
+        }
+      }
+    }
+  }
+`;
+
+export default getAllProductPathsQuery;
