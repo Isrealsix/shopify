a0128414f9871507c1a73d759f23981cae5f7d66
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-06-17 02:12:06 +0700

    fixed css bug

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,5 +1,5 @@
 import { Layout } from "@components/common";
-import { Container } from "@components/ui";
+import { ProductView } from "@components/product";
 import { getConfig } from "@framework/api/config";
 import { getAllProductsPaths, getProduct } from "@framework/product";
 
@@ -38,49 +38,7 @@ export const getStaticProps = async ({
 const ProductSlug = ({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return (
-    <Container>
-      <p>id: {product.id}</p>
-      <p>name: {product.name}</p>
-      <p>price value: {product.price.value}</p>
-      <p>price currency code: {product.price.currencyCode}</p>
-      <p>description: {product.description}</p>
-
-      <h1 className="mb-4">OPTIONS</h1>
-      <div>
-        {product?.options.map((option) => (
-          <div key={option.id}>
-            <p>Name: {option.displayName}</p>
-            {option.values.map((value) => (
-              <div key={value.label}>
-                <p>Label: {value.label}</p>
-                <p>Hex color: {value.hexColor}</p>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      <h1 className="mb-4">VARIANTS</h1>
-      <div>
-        {product?.variants.map((variant) => (
-          <div key={variant.id}>
-            <p>Variant Name: {variant.name}</p>
-            {variant.options.map((variantOption) => (
-              <div key={variantOption.id}>
-                <p>Name {variantOption.displayName}</p>
-                {variantOption?.values.map((value, idx) => (
-                  <div key={idx}>
-                    <p>Label: {value.label}</p>
-                    <p>Hex color: {value.hexColor}</p>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </Container>
-  );
+  return <>{product && <ProductView product={product} />}</>;
 };
 
 export default ProductSlug;
