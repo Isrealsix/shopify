54da1441eb2e5be32369ef50a5fb8daac02be1da
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-06-18 03:45:03 +0700

    added functionality to slide

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,16 +1,32 @@
 import { Children, cloneElement, isValidElement } from "react";
 import styles from "./ProductSlider.module.css";
+import { useKeenSlider } from "keen-slider/react";
 
 interface IProps {
   children: React.ReactNode;
 }
 const ProductSlider: React.FC<IProps> = ({ children }) => {
+  const [sliderRef, _] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(s) {
+      console.log("about to slide", s);
+    },
+  });
+
   return (
     <div className={styles.root}>
-      <div className="keen-slider h-full transition-opacity">
+      <div
+        ref={sliderRef as any}
+        className="keen-slider h-full transition-opacity"
+      >
         {Children.map(children, (child) => {
           if (isValidElement(child)) {
-            return cloneElement(child, { className: "keen-slider__slide" });
+            return cloneElement(child, {
+              className: `${
+                child.props.className ? `${child.props.className}` : ""
+              } keen-slider__slide`,
+            });
           }
         })}
       </div>
