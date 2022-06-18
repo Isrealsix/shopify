c9d60edea1ff87420a2f07998d38a3feaa965d52
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-06-18 17:00:17 +0700

    added helpful comments

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -6,6 +6,11 @@ interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode | React.ReactNode[];
 }
 
+/** This Button component Extends the functionality of usual button
+ * @default Use `<Button>children</Button>`
+ * @argument `children`, `className`, `events`
+ */
+
 const Button: React.FC<IProps> = ({ children, className, ...rest }) => {
   return (
     <button className={cn(style.root, className)} type="button" {...rest}>
