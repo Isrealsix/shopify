0f4c41d3eb696ed389afedc118ee44a4f85cea32
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-06-24 12:51:31 +0700

    fixed Swatch button and addeed logic to display size

--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -1,13 +1,27 @@
 import style from "./Swatch.module.css";
+import { Check } from "@components/icons";
 
 interface IProps {
   color?: string;
   label?: string;
+  variant?: "size" | "color" | string;
 }
 
-const Swatch: React.FC<IProps> = ({ color, label }): JSX.Element => {
+const Swatch: React.FC<IProps> = ({ color, label, variant }): JSX.Element => {
   label = label?.toLowerCase();
-  return <button className={style.roots}>click</button>;
+  variant = variant?.toLowerCase();
+
+  return (
+    <button
+      style={color ? { backgroundColor: color } : {}}
+      className={style.root}
+    >
+      {/* <span>
+        <Check />
+      </span> */}
+      {variant === "size" ? label : null}
+    </button>
+  );
 };
 
 export default Swatch;
